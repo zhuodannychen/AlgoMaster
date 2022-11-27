18b369c9952eefc34ba62ae571338fd21ca07bca
Boyi Shi <adrian.boyishi@gmail.com>
2022-11-26 20:49:11 -0600

    Changed login and register UI

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useNavigate, Navigate } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock, faUser } from '@fortawesome/free-solid-svg-icons';
+import image from '../Assets/images/background.jpg'
 import Axios from 'axios'
 import '../App.css';
 
@@ -33,21 +36,40 @@ function Login() {
   }
 
     return (
-        <div className="Login">
-            <h1>Login Page</h1>
-            <label>Username</label>
-            <input
-                type="text"
-                onChange={(e) => setUsernameLog(e.target.value)} />
-
-            <label>Password</label>
-            <input
-                type="password"
-                onChange={(e) => setPasswordLog(e.target.value)} />
-            
-            <button onClick={login}>Login</button>
-            <h3>{logStatus}</h3>
+        <div className='flex-container' style={{ backgroundImage: `url(${image})`}}>
+        <div className="Login card">
+            <h1 className='title'> AlgoMaster </h1>
+            <form>
+                <div class="form-group">
+                    <label for="email_input" className='mb-2'>Your email </label>
+                    <div className='input-group mb-3' >
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">
+                          <FontAwesomeIcon icon={faUser} style={{fontSize: 24}}/>
+                        </div>
+                      </div>
+                      <input type="email" class="form-control" id="email_input" onChange={e => setUsernameLog(e)} aria-describedby="emailHelp" placeholder="e.g. elon@tesla.com" />
+                    </div>
+                    
+                </div>
+                <div class="form-group">
+                    <label for="password_input" className='mb-2'> Your password </label>
+                    <div className='input-group'>
+                      <div class="input-group-prepend">
+                        <div class="input-group-text">
+                          <FontAwesomeIcon icon={faLock} style={{fontSize: 24}} />
+                        </div>
+                      </div>
+                      <input type="password" class="form-control" id="password_input" onChange={e => setPasswordLog(e)} placeholder="e.g. ilovemangoes123" />
+                    </div>
+                </div>
+            </form>
+            <button id="loginButton" type="button" class="btn btn-primary btn-block mt-3" onClick={login}>
+              <span id="loginButtonText"> Login </span> 
+            </button>
+            <span id='switchToRegister' onClick={() => navigate("register")}> Don't have an account? Register here </span>
         </div>
+    </div>
     );
 }
 
