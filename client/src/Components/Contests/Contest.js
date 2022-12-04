30f5b77273a9a7cc518f34451d51bd0a2eb32559
Danny Chen <zhuodannychen@gmail.com>
2022-12-03 22:41:15 -0600

    conditional rendering signup and enter

--- client/src/Components/Contests/Contest.js
+++ client/src/Components/Contests/Contest.js
@@ -68,7 +68,6 @@ function Contest(props){
   }
 
   const addUserToContest = (username, contest_id) => {
-    console.log(username, contest_id)
     Axios.post("http://localhost:3001/add_user_contest", {username, contest_id})
     .then((response) => {
         if (response.data[0]){
@@ -80,6 +79,20 @@ function Contest(props){
     })
   }
 
+  const checkContestStarted = (start_date) => {
+    const startTS = start_date 
+    const date = new Date()
+
+    const year = zeroPad(date.getUTCFullYear(), 2)
+    const month = zeroPad(date.getUTCMonth()+1, 2)
+    const day = zeroPad(date.getUTCDate(), 2)
+    const hours = zeroPad(date.getUTCHours(), 2)
+    const minutes = zeroPad(date.getMinutes(), 2)
+    const curTS = year + "-" + month + "-" + day + "T" + hours + ":" + minutes
+
+    return startTS <= curTS
+  }
+
   return (
     <div class="card mb-3">
       <div class="card-body">
@@ -89,7 +102,11 @@ function Contest(props){
             <span className='contest_info'>  {parseDate(props.start_date)}, {parseTime(props.start_date)} </span>
             <span id="duration"> {getContestDuration(props.start_date, props.end_date)} hours </span>
             <span id="participants"> {props.participants} participants </span>
+            {checkContestStarted(props.start_date) ? 
+            <button className="btn btn-outline-success" onClick={() => addUserToContest(username, props.contest_id)} style={{textColor: 'green'}} type='button'> Enter </button>
+            :
             <button className="btn btn-outline-success" onClick={() => addUserToContest(username, props.contest_id)} style={{textColor: 'green'}} type='button'> Sign Up </button>
+            }
           </div>
         </div>
       </div>
