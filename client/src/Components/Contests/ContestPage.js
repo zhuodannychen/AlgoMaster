a5398dc764069dd898a2abafc89537632cca3b56
Danny Chen <zhuodannychen@gmail.com>
2022-12-07 16:06:59 -0600

    displays contest name on contest page

--- client/src/Components/Contests/ContestPage.js
+++ client/src/Components/Contests/ContestPage.js
@@ -7,9 +7,15 @@ import "../../App.css"
 function ContestPage(props){
   const [username, setUsername] = useState("");
   const [problems, setProblems] = useState([]);
+  const [contestName, setContestName] = useState("")
   const {contestid} = useParams();
 
   useEffect(() => {
+    Axios.get('http://localhost:3001/contests/' + contestid)
+    .then(response => {
+        setContestName(response.data[0]['contest_name'])        
+    })
+
     Axios.get('http://localhost:3001/contestdetails/'+contestid)
     .then(response => {
         setProblems(response.data[1])
@@ -19,7 +25,7 @@ function ContestPage(props){
 
   return (
     <div class="container">
-        <h1>Contest Page for {contestid}</h1>
+        <h1>Contest Page for {contestName}</h1>
         {console.log(problems)}
         {problems.map((problem, idx) => <li key={idx}><a href={problem['problem_url']}>{problem['problem_name']}</a> {problem['problem_desc']}</li>)}
     </div>
